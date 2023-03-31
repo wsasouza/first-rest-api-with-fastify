beecd7ce7ebd4e9035d8be3349a3bc9cba1cdd7c
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-03-31 09:59:05 -0300

    Feat: Categorizing tests

---
+++ src/test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import { beforeAll, afterAll, describe, it } from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('Should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
